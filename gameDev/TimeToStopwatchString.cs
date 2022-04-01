af1ffb5fba0b92f2cdf7415c848f1c7b78fbf63c
Christian Engelsberger <102614132+engelsberger@users.noreply.github.com>
2022-04-01 10:57:37 +0200

    Create TimeToStopwatchString.cs

---
+++ gameDev/TimeToStopwatchString.cs
@@ -0,0 +1,14 @@
+
+
+private string TimeToStopwatchString(float time)
+{
+    /* TIME IN SECONDS (corresponding to Time.deltaTime by Unity)
+     * Returns given time in format 00:00:00 (min:sec:millisec) as string.
+     */
+
+    int minutes = Mathf.FloorToInt(time / 60F);
+    int seconds = Mathf.FloorToInt(time % 60F);
+    int milliseconds = Mathf.FloorToInt((time * 100F) % 100F);
+
+    return minutes.ToString("00") + ":" + seconds.ToString("00") + ":" + milliseconds.ToString("00");
+}
