ff7a939a779bf61c3594b001de173f644c07ab5a
C. Engelsberger <102614132+engelsberger@users.noreply.github.com>
2022-04-20 15:31:38 +0200

    Update StringToIntAdvanced.cs

--- parse/StringToIntAdvanced.cs
+++ parse/StringToIntAdvanced.cs
@@ -6,13 +6,15 @@
  * Exception: "and" can be added (ex. four hundred and twenty) and still be valid.
  */
 
-public static int ParseInt(string s)
+static int ParseInt(string s)
 {
     // Assign numbers to certain words
     string[] numData = new string[] {
         "1 one", "2 two", "3 three", "4 four", "5 five",
         "6 six", "7 seven", "8 eight", "9 nine",
-        "10 ten", "20 twenty", "30 thirty", "40 fourty", "50 fifty",
+        "10 ten", "11 eleven", "12 twelve", "13 thirteen", "14 fourteen",
+        "15 fifteen", "16 sixteen", "17 seventeen", "18 eighteen", "19 nineteen",
+        "20 twenty", "30 thirty", "40 fourty", "50 fifty",
         "60 sixty", "70 seventy", "80 eighty", "90 ninety"
     };
 
@@ -21,7 +23,7 @@ public static int ParseInt(string s)
     if (s.ToLower().Contains("billion")) return 1000000000;
 
     // Validate Input
-    if (s.ToLower().Split(' ').Where(s => 
+    if (s.ToLower().Split(' ').Where(s =>
         numData.Where(n => s.Contains('-') ? (n.Contains(s.Split('-')[0]) && numData.Where(m => m.Contains(s.Split('-')[1])).Count() > 0) : n.Contains(s)).Count() == 0
         && !s.Equals("hundred") && !s.Equals("thousand") && !s.Equals("million") && !s.Equals("and"))
         .Count() > 0) return -1;
@@ -47,7 +49,9 @@ public static int ParseInt(string s)
         {
             mod = input[i].Equals("hundred") ? 1 : input[i].Equals("thousand") ? 2 : input[i].Equals("million") ? 4 : 0;
             if (input[i].Equals("hundred") && i + 1 < input.Length && input[i + 1].Equals("thousand")) mod = 3;
+            else if (input[i].Equals("hundred") && i + 2 < input.Length && input[i + 2].Equals("thousand")) mod = 3;
             if (input[i].Equals("hundred") && i + 1 < input.Length && input[i + 1].Equals("million")) mod = 5;
+            else if (input[i].Equals("hundred") && i + 2 < input.Length && input[i + 2].Equals("million")) mod = 5;
         }
 
         output += num * (mod == 1 ? 100 : mod == 2 ? 1000 : mod == 3 ? 100000 : mod == 4 ? 1000000 : mod == 5 ? 100000000 : 1);
